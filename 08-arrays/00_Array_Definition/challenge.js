da12d2c2158f83af075c32a0d28448f68c0d2a78
rickyplum <richard.plum@brandingbrand.com>
2015-01-22 06:03:47 +0000

    updates to 00, 01

--- 08-arrays/00_Array_Definition/challenge.js
+++ 08-arrays/00_Array_Definition/challenge.js
@@ -1,3 +1,10 @@
-module.exports.newArray = undefined;
+module.exports.newArray = function (num1, num2, num3, num4) {
+	var anArray = [num1, num2, num3, num4];
+	return anArray;
+}
 
-module.exports.firstAndLast = undefined;
+module.exports.firstAndLast = function (a) {
+  var lastIndex = a.length - 1;
+  var newArray = [a[0], a[lastIndex]];
+  return newArray;
+}
--- 08-arrays/01_Array_Functions/challenge.js
+++ 08-arrays/01_Array_Functions/challenge.js
@@ -1,5 +1,28 @@
-module.exports.reversePlusOne = undefined;
+module.exports.reversePlusOne = function(array) {
+  if (array.length < 3) {
+    var smallArray = [array[1], array[0]]
+    smallArray.unshift(1);
+    return smallArray;
+  }
+  else {
+    var lastIndex = array.length - 1;
+    var secondLastIndex = array.length - 2;
+    var largeArray = [array[lastIndex], array[secondLastIndex], array[1], array[0]];
+    largeArray.unshift(1);
+    return largeArray;
+  }
+}
 
-module.exports.plusesEverywhere = undefined;
+module.exports.plusesEverywhere = function (anArray) {
+  var arraySize = anArray.length;
+  if (arraySize < 3) {
+    return anArray[0] + "+" + anArray[1];
+  }
+  else {
+    return anArray[0] + "+" + anArray[1] + "+" + anArray[2] + "+" + anArray[3] + "+" + anArray[4];
+  }
+}
 
-module.exports.arrayQuantityPlusOne = undefined;
+module.exports.arrayQuantityPlusOne = function (arrayOfNums) {
+  return arrayOfNums.length + 1;
+}
