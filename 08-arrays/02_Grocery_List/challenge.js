756887d6a1b089698a9bcaa69193a77bc5d5c327
rickyplum <richard.plum@brandingbrand.com>
2015-01-23 06:55:49 +0000

    updating grocery list

--- 08-arrays/02_Grocery_List/challenge.js
+++ 08-arrays/02_Grocery_List/challenge.js
@@ -1,3 +1,26 @@
-module.exports.addItem = undefined;
+module.exports.addItem = function (arg1, arg2) {
+if (arg1 == arg2[0] || arg1 == arg2[1] || arg1 == arg2[2] || arg1 == arg2[3]) {
+    return arg2;
+  }
+  else {
+    arg2.push(arg1);
+    return arg2;
+  }
+}
 
-module.exports.reverseSortedList = undefined;
+module.exports.reverseSortedList = function (anArray) {
+  
+  var newArray = anArray.sort(function (a, b) {
+    if (a > b) {
+      return -1;
+    }
+    else if (a < b) {
+      return 1;
+    }
+    else if (a == b) {
+      return 0;
+    }
+  });
+
+  return newArray;
+}
