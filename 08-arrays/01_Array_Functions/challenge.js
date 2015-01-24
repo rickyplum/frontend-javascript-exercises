b9caddda21adb560c24597635e4020dbd31efb29
rickyplum <richard.plum@brandingbrand.com>
2015-01-24 07:01:07 +0000

    01 arrays fix

--- 08-arrays/01_Array_Functions/challenge.js
+++ 08-arrays/01_Array_Functions/challenge.js
@@ -1,28 +1,16 @@
-module.exports.reversePlusOne = function(array) {
-  if (array.length < 3) {
-    var smallArray = [array[1], array[0]]
-    smallArray.unshift(1);
-    return smallArray;
-  }
-  else {
-    var lastIndex = array.length - 1;
-    var secondLastIndex = array.length - 2;
-    var largeArray = [array[lastIndex], array[secondLastIndex], array[1], array[0]];
-    largeArray.unshift(1);
-    return largeArray;
-  }
+module.exports.reversePlusOne = function(r) {
+  var array = r;
+  array.reverse();
+  array.unshift(1);
+  return array;
 }
 
-module.exports.plusesEverywhere = function (anArray) {
-  var arraySize = anArray.length;
-  if (arraySize < 3) {
-    return anArray[0] + "+" + anArray[1];
-  }
-  else {
-    return anArray[0] + "+" + anArray[1] + "+" + anArray[2] + "+" + anArray[3] + "+" + anArray[4];
-  }
+module.exports.plusesEverywhere = function(r) {
+  var array = r;
+  return array.join('+');
 }
 
-module.exports.arrayQuantityPlusOne = function (arrayOfNums) {
-  return arrayOfNums.length + 1;
+module.exports.arrayQuantityPlusOne = function(r) {
+  var array = r;
+  return array.length + 1;
 }
