5d2a10039dc302398b1ba85b05d6e7afeeedb8dc
rickyplum <richard.plum@brandingbrand.com>
2015-01-25 23:48:04 +0000

    updating loops

--- 10-loops/00-looping-over-arrays/challenge.js
+++ 10-loops/00-looping-over-arrays/challenge.js
@@ -1,5 +1,23 @@
-module.exports.sumNumbers = undefined;
+module.exports.sumNumbers = function(numbers) {
+  var sum = 0;
+  for (var i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+  }
+  return sum;
+};
 
-module.exports.splitAndLowerCaseString = undefined;
+module.exports.splitAndLowerCaseString = function(inputString) {
+  var output = inputString.split(',');
+  for (var i = 0; i < output.length; i++) {
+    output[i] = output[i].toLowerCase();
+  }
+  return output;
+};
 
-module.exports.addIndex = undefined;
+module.exports.addIndex = function(arrayOfData) {
+  var output = [];
+  for (var i = 0; i < arrayOfData.length; i++) {
+    output.push(i + " is " + arrayOfData[i]);
+  }
+  return output;
+};
