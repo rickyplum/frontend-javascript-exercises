7d8c5536daa02e83373681ad11c75110b9163d24
Ricky Plum <Ricky@Rickys-MacBook-Air.local>
2015-01-27 20:18:09 -0500

    01-iterating over an object

--- 10-loops/01-iterating-over-an-object/challenge.js
+++ 10-loops/01-iterating-over-an-object/challenge.js
@@ -1,5 +1,24 @@
-module.exports.getKeys = undefined;
+module.exports.getKeys = function(object) {
+  var myArray = [];
+  for (var prop in object) {
+    myArray.push(prop);
+  }
+  return myArray;
+};
 
-module.exports.getValues = undefined;
+module.exports.getValues = function(object) {
+  var myArray = [];
+  for (var prop in object) {
+    myArray.push(object[prop]);
+  }
+  return myArray;
+};
 
-module.exports.objectToArray = undefined;
+module.exports.objectToArray = function(object) {
+  var myArray = [];
+  for (var prop in object) {
+    var stringed = prop + " is " + object[prop];
+    myArray.push(stringed);
+  }
+  return myArray;
+};
